7cdb1c65aa4387c420d39f8830e759e922b95aad
Gunnar A. Holst <Gunnar A. Holst>
2016-11-04 17:07:52 +0100

    New box and shelf-model. Optimized and rewrote Room, Shelf and BoxesMan-scripts.

--- VRProsjekt_Gruppe7/Assets/Scripts/RoomScripts/RoomGenerator.cs
+++ VRProsjekt_Gruppe7/Assets/Scripts/RoomScripts/RoomGenerator.cs
@@ -6,12 +6,11 @@ public class RoomGenerator : MonoBehaviour
     // Left -> Front -> Right
     public GameObject ShelfPrefab;
 
-    private Vector3[] _shelvesPositions = { new Vector3(-0.9f, 0, 0), new Vector3(0, 0, 0.9f), new Vector3(0.9f, 0, 0) };
     private GameObject[] _shelves;
 
     void Awake()
     {
-        _shelves = new GameObject[_shelvesPositions.Length];
+        _shelves = new GameObject[RoomValues.ShelvesPositions.Length];
     }
     /*
     void Start()
@@ -25,30 +24,25 @@ public class RoomGenerator : MonoBehaviour
         GetComponent<BoxContentsManager>().Init();
 
         SpawnShelves();
+        GetComponent<BoxesManager>().GenerateBoxes(_shelves);
+        /*
         GetComponent<BoxContentsManager>().FillBoxes(
             GetComponent<BoxesManager>().GenerateBoxes(_shelves),
             GetComponent<BoxesManager>().GetBoxDimesions());
+            */
     }
 
     private void SpawnShelves()
     {
-        for (int i = 0; i < _shelvesPositions.Length; i++)
+        for (int i = 0; i < RoomValues.ShelvesPositions.Length; i++)
         {
-            //Debug.Log("Spawning shelf nr: " + i);
-            RoomSide side = GetRoomSide(i);
-
             _shelves[i] = (GameObject)Instantiate(ShelfPrefab, transform.position, transform.rotation);
-            _shelves[i].GetComponent<ShelfManager>().PlaceObject(_shelvesPositions[i], 2f, side);
+            _shelves[i].GetComponent<ShelfManager>().PlaceShelf(RoomValues.ShelvesPositions[i], GetRoomSide(i));
         }
     }
 
     private RoomSide GetRoomSide(int i)
     {
-        if (i == 0)
-            return RoomSide.Left;
-        else if (i == 2)
-            return RoomSide.Right;
-        else
-            return RoomSide.Front;
+        return (i == 0) ? RoomSide.Left : RoomSide.Right;
     }
 }
--- VRProsjekt_Gruppe7/Assets/Scripts/RoomScripts/ShelfManager.cs
+++ VRProsjekt_Gruppe7/Assets/Scripts/RoomScripts/ShelfManager.cs
@@ -5,33 +5,20 @@ using System;
 public enum RoomSide
 {
     Left,
-    Front,
-    Right
+    Right,
+    Front
 }
 
 public class ShelfManager : MonoBehaviour {
 
     public GameObject[] Legs;
 
-    private readonly int _nrOfShelfFloors = 3;
-    private readonly float _spaceBetweenShelves = 0.5f;
-    private readonly float _lowestFloorPosY = 0.11f;
+    private readonly int _nrOfShelfFloors = 4;
 
-    private readonly float _legsDiameter = 0.03f;
-    private readonly float _legsHeight = 0.85f;
-
-    private readonly float _legsOffsetX = 0.45f;
-    private readonly float _legsOffsetY = 0.85f;
-    private readonly float _legsOffsetZ = 0.115f;
-
-    public void PlaceObject(Vector3 pos, float width, RoomSide side)
+    public void PlaceShelf(Vector3 pos, RoomSide side)
     {
         transform.position = pos;
-        transform.localScale = new Vector3(width, transform.localScale.y, transform.localScale.z);
-
-        AdjustSizes(width);
         RotateShelf(side);
-
     }
 
     private void RotateShelf(RoomSide side)
@@ -40,31 +27,11 @@ public class ShelfManager : MonoBehaviour {
             return;
 
         float rotY = (side == RoomSide.Left) ? -90f : 90f;
-        transform.Rotate(0f, rotY, 0f);
-    }
-
-    private void AdjustSizes(float shelfWidth)
-    {
-        foreach(GameObject leg in Legs)
-        {
-            float newDia = _legsDiameter / shelfWidth;
-
-            leg.transform.localScale = new Vector3(newDia, transform.localScale.y, newDia);
-        }
+        transform.Rotate(270f, rotY + 180f, 0f);
     }
 
     public int GetNrOfShelfFloors()
     {
         return _nrOfShelfFloors;
-    }
-
-    public float GetSpaceBetweenShelfFloors()
-    {
-        return _spaceBetweenShelves;
-    }
-
-    public float GetLowestShelfFloorY()
-    {
-        return _lowestFloorPosY;
     }
 }
