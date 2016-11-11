1f940c159f5bd29380932673b4cf9d9bc2e5359c
GunnarA <GunnarA@GUNNARLAPTOP>
2016-11-11 12:08:00 +0100

    Added highscore, new corridor and door.

--- VRProsjekt_Gruppe7/Assets/Scripts/HighScoreController.cs
+++ VRProsjekt_Gruppe7/Assets/Scripts/HighScoreController.cs
@@ -5,8 +5,25 @@ using System.Collections;
 public class HighScoreController : MonoBehaviour
 {
     public Text[] HighScoreText;
-	
-	void Start ()
+    private int _initScoreValue = 0;
+
+    // Debug variables
+    public bool UpdateNewScore = false;
+    public int NewTestScore = 0;
+
+    void Update()
+    {
+        if (UpdateNewScore)
+        {
+            UpdateNewScore = false;
+            GameEnd(NewTestScore);
+        }
+    }
+
+
+    // Debug end
+
+    void Start ()
 	{
 	    Init();
 	}
@@ -15,13 +32,90 @@ public class HighScoreController : MonoBehaviour
     {
         for (int i = 1; i <= 10; i++)
         {
-            print(i);
-            string index = (i < 10) ? ("0" + i) : "" + i;  
-            HighScoreText[i - 1].text = index + ": " + (10 - i + "00");
+            string index = GetPosIndex(i);
+            HighScoreText[i - 1].GetComponent<Text>().color = Color.white;
+
+            if (!PlayerPrefs.HasKey(index))
+            {
+                PlayerPrefs.SetInt(index, _initScoreValue);
+                HighScoreText[i - 1].text = index + ": " + _initScoreValue;
+            }
+            else
+            {
+                HighScoreText[i - 1].text = index + ": " + PlayerPrefs.GetInt(index);
+            }
         }
     }
 
-    void Update () {
-	
-	}
+    public void GameEnd(int playerScore)
+    {
+        int currentScore = playerScore;
+        int[] scores = new int[HighScoreText.Length];
+        bool newHighScore = false;
+        int newHighScorePos = 10;        
+
+        for (int i = 0; i < scores.Length; i++)
+        {
+            print("GameEndLoop: " + i);
+
+            string index = GetPosIndex(i + 1);
+            scores[i] = PlayerPrefs.GetInt(index);
+
+            if (scores[i] < currentScore && !newHighScore)
+            {
+                newHighScore = true;
+                newHighScorePos = i;
+            }
+        }
+
+        if (newHighScore)
+        {
+            scores = AddNewScore(scores, currentScore, newHighScorePos);
+            UpdateHighScoreBoard(scores, newHighScorePos);
+        }
+    }
+
+    private void UpdateHighScoreBoard(int[] scores, int newHighScorePos)
+    {
+        for (int i = 0; i < scores.Length; i++)
+        {
+            print("UpdateHighScoreBoard: " + i);
+
+            string index = GetPosIndex(i + 1);
+            HighScoreText[i].text = index + ": " + PlayerPrefs.GetInt(index);
+
+            HighScoreText[i].GetComponent<Text>().color = (i == newHighScorePos) ? Color.green : Color.white;
+        }
+    }
+
+    
+
+    private int[] AddNewScore(int[] currentHighScores, int newScore, int pos)
+    {
+        int[] newScores = currentHighScores;
+
+        for (int i = currentHighScores.Length - 1; i >= pos; i--)
+        {
+            print("AddNewScore: " + i);
+            // Current key
+            string index = GetPosIndex(i + 1);
+
+            // Move highscore at next pos to current pos
+            if (i == pos)
+            {
+                PlayerPrefs.SetInt(index, newScore);
+                newScores[i] = newScore;
+                break;
+            }
+
+            PlayerPrefs.SetInt(index, currentHighScores[i - 1]);
+            newScores[i] = newScores[i - 1];
+        }
+        return newScores;
+    }
+
+    private string GetPosIndex(int i)
+    {
+        return (i < 10) ? ("0" + i) : "" + i;
+    }
 }
