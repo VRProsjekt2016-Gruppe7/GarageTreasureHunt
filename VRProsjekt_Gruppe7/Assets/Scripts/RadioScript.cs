c8a684367f1d1f598eebd25f61333623f1d4d6f8
GunnarA <GunnarA@GUNNARLAPTOP>
2016-11-11 12:46:40 +0100

    Fixed highscores to interact with the new HighScoreModel. Removed redundant prefabs.

--- VRProsjekt_Gruppe7/Assets/Scripts/RadioScript.cs
+++ VRProsjekt_Gruppe7/Assets/Scripts/RadioScript.cs
@@ -6,38 +6,36 @@ public class RadioScript : MonoBehaviour {
 
     public float MaxRotationalForce = 0.1f;
     public float MaxRotate = 5f;
-    public double bpm = 140.0F;
-    public int jumpTo_bpmRatio = 4;
-    private double nextTick = 0.0F;
-    private double sampleRate = 0.0F;
+    public double Bpm = 140.0F;
+    public int JumpToBpmRatio = 4;
+    private double _nextTick = 0.0F;
+    private double _sampleRate = 0.0F;
 
-    private bool running = false;
-    private Rigidbody rb;
+    private bool _running = false;
     private bool _jump = false;
 
-    //For the audio drop on collision
-    private float pitch;
-    float counter = 0;
-    AudioSource audio;
-    bool broken = false;
-    public float durationOfDeath = 500;
-    public float MaxImpactForceBeforeBroken = 2;
+    //For the _radioAudioSource drop on collision
+    private float _pitch;
+    private float _counter = 0;
+    AudioSource _radioAudioSource;
+    private bool _broken = false;
+    private float _durationOfDeath = 500;
+    private float _maxImpactForceBeforeBroken = 2;
 
-    //The sound is supposed to speed up a teeeensy bit when the cloc is running low
-    GameManager GM_script;
+    //The sound is supposed to speed up a teeeensy bit when the cloc is _running low
+    GameManager _gM;
 
     void Start()
     {
-        GM_script = GameObject.Find("_SCRIPTS").GetComponent<GameManager>();
+        _gM = GameObject.Find("_SCRIPTS").GetComponent<GameManager>();
         double startTick = AudioSettings.dspTime;
-        sampleRate = AudioSettings.outputSampleRate;
-        nextTick = startTick * sampleRate;
-        running = true;
-        rb = gameObject.GetComponent<Rigidbody>();
+        _sampleRate = AudioSettings.outputSampleRate;
+        _nextTick = startTick * _sampleRate;
+        _running = true;
 
         //For breaking
-        audio = GetComponent<AudioSource>();
-        pitch = audio.pitch;
+        _radioAudioSource = GetComponent<AudioSource>();
+        _pitch = _radioAudioSource.pitch;
 
 
         
@@ -49,60 +47,59 @@ public class RadioScript : MonoBehaviour {
         {
             _jump = false;
             Jump();
-            MaxRotationalForce = MaxRotationalForce * pitch; //this is so that when the radio dies, the jumping stops
+            MaxRotationalForce = MaxRotationalForce * _pitch; //this is so that when the radio dies, the jumping stops
             
         }
         // increasing stress if game is near the end --- did mess with gameManager script to make this happen
-        Debug.Log("GM_script.Get_timeLeft is: " + GM_script.Get_timeLeft());
 
-        if (GM_script.Get_timeLeft() <= 15 && GM_script.Get_timeLeft() > 0)
+        if (_gM.Get_timeLeft() <= 15 && _gM.Get_timeLeft() > 0)
         {
-            audio.pitch = 1.05f;
+            _radioAudioSource.pitch = 1.05f;
         }
-        else if (!broken) { audio.pitch = 1; }
+        else if (!_broken) { _radioAudioSource.pitch = 1; }
 
-        //Debug.Log("the radio is broken == " + broken);
-        if (broken)
+        //Debug.Log("the radio is _broken == " + _broken);
+        if (_broken)
         {
-            audio.pitch = Mathf.Lerp(1f, 0, Mathf.Lerp(0f, 1, counter / durationOfDeath));
-            //Debug.Log("counter is: " + counter + " and durationOfDeath is: " + durationOfDeath);
-            //Debug.Log("Therefore counter/durationofdeath is: " + counter / durationOfDeath);
-            //Debug.Log("Therefore Lerp 1f -> 0 is at " + (Mathf.Lerp(0, 1, counter/durationOfDeath)));
-        } else counter = 0;
-        counter++;
-        pitch = audio.pitch;
-        if (counter > durationOfDeath) { Destroy(this); }
-        //if (Input.GetButtonDown("Jump")) { broken = true; }
+            _radioAudioSource.pitch = Mathf.Lerp(1f, 0, Mathf.Lerp(0f, 1, _counter / _durationOfDeath));
+            //Debug.Log("_counter is: " + _counter + " and _durationOfDeath is: " + _durationOfDeath);
+            //Debug.Log("Therefore _counter/durationofdeath is: " + _counter / _durationOfDeath);
+            //Debug.Log("Therefore Lerp 1f -> 0 is at " + (Mathf.Lerp(0, 1, _counter/_durationOfDeath)));
+        } else _counter = 0;
+        _counter++;
+        _pitch = _radioAudioSource.pitch;
+        if (_counter > _durationOfDeath) { Destroy(this); }
+        //if (Input.GetButtonDown("Jump")) { _broken = true; }
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.relativeVelocity.magnitude > MaxImpactForceBeforeBroken)
-            broken = true;
+        if (collision.relativeVelocity.magnitude > _maxImpactForceBeforeBroken)
+            _broken = true;
     }
 
     void Jump()
     {
 
-        GetComponent<Rigidbody>().AddForce(Vector3.up * pitch, ForceMode.Impulse);
+        GetComponent<Rigidbody>().AddForce(Vector3.up * _pitch, ForceMode.Impulse);
         GetComponent<Rigidbody>().AddRelativeTorque(new Vector3(0, 0, Random.Range(-MaxRotationalForce, MaxRotationalForce)), ForceMode.Impulse);
 
     }
 
     void OnAudioFilterRead(float[] data, int channels)
     {
-        if (!running)
+        if (!_running)
             return;
 
-        double samplesPerTick = sampleRate * 60.0F / (bpm * pitch) * 4.0F / jumpTo_bpmRatio;
-        double sample = AudioSettings.dspTime * sampleRate;
+        double samplesPerTick = _sampleRate * 60.0F / (Bpm * _pitch) * 4.0F / JumpToBpmRatio;
+        double sample = AudioSettings.dspTime * _sampleRate;
         int dataLen = data.Length / channels;
         int n = 0;
         while (n < dataLen)
         {
-            while (sample + n >= nextTick)
+            while (sample + n >= _nextTick)
             {
-                nextTick += samplesPerTick;
+                _nextTick += samplesPerTick;
                 _jump = true;
                 
             }
