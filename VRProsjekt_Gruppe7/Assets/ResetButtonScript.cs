27a7977af575737e98523a6effaadfb0653b6495
Mntronslien <mntronslien@gmail.com>
2016-11-23 17:30:09 +0100

    Added a button for resetting the game.

---
+++ VRProsjekt_Gruppe7/Assets/ResetButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using NewtonVR;
+
+public class ResetButtonScript : MonoBehaviour {
+
+    public NVRButton Button;
+
+    GameManager _gM;
+
+    void Start()
+    {
+        _gM = GameObject.Find("_SCRIPTS").GetComponent<GameManager>();
+    }
+
+    private void Update()
+    {
+        if (Button.ButtonDown)
+        {
+            
+        //RESET GAME METHOIS TO BE CALLED HERE
+
+        }
+    }
+}
